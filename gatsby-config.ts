c1b929c9e0478c89cd01c0b5211bd598fc7c6936
peterchu999 <peter.andrew987@gmail.com>
2022-04-09 14:47:42 +0700

    Initial commit from gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,11 @@
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `book-highlight`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-react-helmet"]
+};
+
+export default config;
