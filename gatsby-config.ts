7f8c1fd02fc3f2797349f53520ce33c4f2d8e1e4
peterchu999 <peter.andrew987@gmail.com>
2022-04-09 19:53:12 +0700

    change gatsby api to netlify function

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -5,7 +5,7 @@ const config: GatsbyConfig = {
     title: `book-highlight`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ["gatsby-plugin-react-helmet", "gatsby-plugin-netlify"],
+  plugins: ["gatsby-plugin-react-helmet"],
 };
 
 export default config;
